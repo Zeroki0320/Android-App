37584863aa297dc4d934fd7405402f7962bac45c
Zeroki0320 <lhlant@gmail.com>
2017-06-12 13:23:07 +0800

    lab 6 task 2 (unfinish)

--- Lab_06 - Partial Program/IntentResultsExercise/app/src/main/java/com/exercise/android/intentresultsexercise/MainActivity.java
+++ Lab_06 - Partial Program/IntentResultsExercise/app/src/main/java/com/exercise/android/intentresultsexercise/MainActivity.java
@@ -7,27 +7,47 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Timer;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
 
 public class MainActivity extends ActionBarActivity {
     private static final int REQUEST_CODE = 3434;
     private int trial = 0;
+    Intent act ;
+    Timer timer = new Timer();
+    double starttime;
+    private TextView TVmsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TVmsg = (TextView)findViewById(R.id.TVmsg);
     }
 
-    public void clickHandler(View view) {
-        // ????
+    public void run(){
+        double millis = System.currentTimeMillis() - starttime;
+        int seconds = (int) (millis / 1000);
+        int minutes = seconds / 60;
+        seconds     = seconds % 60;
+
+        TVmsg.setText(String.format("%d:%02d", minutes, seconds));
     }
+
+
+        public void clickHandler (View view){
+
+        }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
             if (data.hasExtra("playTime")) {
                 TextView tv = (TextView) findViewById(R.id.TVmsg);
-                // ????
+
             }
         }
     }
