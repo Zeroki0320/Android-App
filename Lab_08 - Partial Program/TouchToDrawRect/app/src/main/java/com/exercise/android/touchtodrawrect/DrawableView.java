87be6694b12e01e4e5ffa2f39a38e5ef402e8d30
Zeroki0320 <lhlant@gmail.com>
2017-06-19 13:27:55 +0800

    lab 08 part 1

--- Lab_08 - Partial Program/TouchToDrawRect/app/src/main/java/com/exercise/android/touchtodrawrect/DrawableView.java
+++ Lab_08 - Partial Program/TouchToDrawRect/app/src/main/java/com/exercise/android/touchtodrawrect/DrawableView.java
@@ -20,6 +20,8 @@ public class DrawableView extends View {
     float x2, y2; // second corner
 
     Paint paint = new Paint();
+    RectF rect = new RectF();
+    int color_idx;
 
     public DrawableView(Context context) {
         super(context);
@@ -31,6 +33,8 @@ public class DrawableView extends View {
         super.onDraw(canvas);
         /** COMPLETE THIS PART **/
         // Draw a rectangle of opposite corners (x1, y1) and (x2, y2) with property & paint
+        super.onDraw(canvas);
+        paint.setColor(color_idx);
         canvas.drawRect(x1, y1, x2, y2, paint);
     }
 
@@ -45,12 +49,18 @@ public class DrawableView extends View {
 
             /** COMPLETE THIS PART **/
             // update x2 and y2
+            x2 = event.getX();
+            y2 = event.getY();
+
 
         } else if (event.getAction() == MotionEvent.ACTION_UP) {
 
             /** COMPLETE THIS PART **/
             // update x2 and y2
             // set the paint color to blue
+            x2 = event.getX();
+            y2 = event.getY();
+            color_idx = Color.BLUE;
 
         }
         Log.d("Event", "point: " + x1 + " " + y1 + " " + x2 + " " + y2);
